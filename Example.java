053ceab37ae9d45bf7686acced0151b3c6a28935
rbekeris <bekeris.code@gmail.com>
2022-11-06 20:49:44 +0100

    First test code

---
+++ Example.java
@@ -0,0 +1,9 @@
+/*
+ This ia multiline comment
+ */
+class Example {
+    //A Java program begins with a call to main()
+    public static void main(String[] args){
+        System.out.println("Java Test number one...");
+    }
+}
